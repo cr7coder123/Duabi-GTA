d5b048008cfe0ec8b39fafb87e7e798a2c22a5bc
Rayyan Goraya <126843353+cr7coder123@users.noreply.github.com>
2025-01-27 17:53:33 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,12 +3,12 @@ var inCar=false;
 var carType="player"
 
 function preload() {
-  playerIMG = loadImage("./Images/player.png");
-  playerAn = loadAnimation(".Images/playerMove1.png", "./Images/playerMove2.png");
+  playerIMG = loadImage("../Images/player.png");
+  playerAn = loadAnimation("../Images/playerMove1.png", "../Images/playerMove2.png");
   
   ferrariIMG=loadImage("./Images/Ferrari.png")
 
-  dubaiIMG=loadImage("./Images/Dubai.png")
+  dubaiIMG=loadImage("../Images/Dubai.png")
 
 }
 
@@ -100,100 +100,37 @@ function draw() {
 
   if (keyDown("right") || keyDown("d")) {
       player.rotation = 90; 
-      //moving the roads
-      Sea.x-=moveNum;
-      JumRoad.x-=moveNum;
-      WaslRoad.x-=moveNum;
-      ZayedRoad.x-=moveNum;
-      street1.x-=moveNum;
-      street2.x-=moveNum;
-      street3.x-=moveNum;
-      street4.x-=moveNum;
-      street5.x-=moveNum;
-      street6.x-=moveNum;
-      street7.x-=moveNum;
-      street8.x-=moveNum;
-      street9.x-=moveNum;
-      street10.x-=moveNum;
-      EmiratesRoad.x-=moveNum;
-      SweihanRoad.x-=moveNum;
-      SultanRoad.x-=moveNum;
-      //Cars
+      moveWorld(-moveNum, 0);
       if (inCar===true){
         ferrari.x=player.x
         ferrari.rotation=90
       } else{
       ferrari.x -= moveNum;
+      carType="player"
       }
     } else if (keyDown("left") || keyDown("a")) {
       player.rotation = -90; 
-      Sea.x+=moveNum;
-      JumRoad.x+=moveNum; 
-      WaslRoad.x+=moveNum;
-      ZayedRoad.x+=moveNum;
-      street1.x+=moveNum;
-      street2.x+=moveNum;
-      street3.x+=moveNum;
-      street4.x+=moveNum;
-      street5.x+=moveNum;
-      street6.x+=moveNum;
-      street7.x+=moveNum;
-      street8.x+=moveNum;
-      street9.x+=moveNum;
-      street10.x+=moveNum;
-      EmiratesRoad.x+=moveNum;
-      SweihanRoad.x+=moveNum;
-      SultanRoad.x+=moveNum;
+      moveWorld(moveNum, 0);
       if (inCar===true){
         ferrari.x=player.x
         ferrari.rotation=-90
       } else{
       ferrari.x += moveNum; 
+      carType="player"
       } 
     } else if (keyDown("up") || keyDown("w")) {
       player.rotation = 0;  
-      Sea.y+=moveNum; 
-      JumRoad.y+=moveNum;
-      WaslRoad.y+=moveNum;
-      ZayedRoad.y+=moveNum;
-      street1.y+=moveNum;
-      street2.y+=moveNum;
-      street3.y+=moveNum;
-      street4.y+=moveNum;
-      street5.y+=moveNum;
-      street6.y+=moveNum;
-      street7.y+=moveNum;
-      street8.y+=moveNum;
-      street9.y+=moveNum;
-      street10.y+=moveNum;
-      EmiratesRoad.y+=moveNum;
-      SweihanRoad.y+=moveNum;
-      SultanRoad.y+=moveNum;
+      moveWorld(0, moveNum);
       if (inCar===true){
         ferrari.y=player.y
         ferrari.rotation=0
       } else{
       ferrari.y += moveNum;
+      carType="player"
       }
     } else if (keyDown("down") || keyDown("s")) {
       player.rotation = 180; 
-      Sea.y-=moveNum;
-      JumRoad.y-=moveNum; 
-      WaslRoad.y-=moveNum;
-      ZayedRoad.y-=moveNum;
-      street1.y-=moveNum;
-      street2.y-=moveNum;
-      street3.y-=moveNum;
-      street4.y-=moveNum;
-      street5.y-=moveNum;
-      street6.y-=moveNum;
-      street7.y-=moveNum;
-      street8.y-=moveNum;
-      street9.y-=moveNum;
-      street10.y-=moveNum;
-      EmiratesRoad.y-=moveNum;
-      SweihanRoad.y-=moveNum;
-      SultanRoad.y-=moveNum;
+      moveWorld(0, -moveNum);
       if (inCar===true){
         ferrari.y=player.y
         ferrari.rotation=180
@@ -202,6 +139,8 @@ function draw() {
         carType="player"
       }
     }
+
+
 
     if (player.collide(ferrari)){
       carType="laFerrari"
@@ -226,9 +165,111 @@ function draw() {
   } else {
     player.changeAnimation("standing");
   }
+ 
 
   drawSprites();
+
+  drawMiniMap(moveNum);
+
+
   text(moveNum,20, windowHeight-20)
   text(carType, 20, windowHeight-50)
 }
 
+
+function moveWorld(dx, dy) {
+  Sea.x += dx;
+  Sea.y += dy;
+  JumRoad.x += dx;
+  JumRoad.y += dy;
+  WaslRoad.x += dx;
+  WaslRoad.y += dy;
+  ZayedRoad.x += dx;
+  ZayedRoad.y += dy;
+  EmiratesRoad.x += dx;
+  EmiratesRoad.y += dy;
+  SweihanRoad.x += dx;
+  SweihanRoad.y += dy;
+  SultanRoad.x += dx;
+  SultanRoad.y += dy;
+  street1.x += dx;
+  street1.y += dy;
+  street2.x += dx;
+  street2.y += dy;
+  street3.x += dx;
+  street3.y += dy;
+  street4.x += dx;
+  street4.y += dy;
+  street5.x += dx;
+  street5.y += dy;
+  street6.x += dx;
+  street6.y += dy;
+  street7.x += dx;
+  street7.y += dy;
+  street8.x += dx;
+  street8.y += dy;
+  street9.x += dx;
+  street9.y += dy;
+  street10.x += dx;
+  street10.y += dy;
+}
+function drawMiniMap() {
+  // Mini-map dimensions and position
+  let mapWidth = 200; // Width of the mini-map
+  let mapHeight = 150; // Height of the mini-map
+  let mapX = width - mapWidth - 20; // Mini-map's top-left X position
+  let mapY = height - mapHeight - 20; // Mini-map's top-left Y position
+
+  // Scaling factors to fit the real-world map into the mini-map
+  let scaleX = mapWidth / 48500; // Scale for X axis based on world width
+  let scaleY = mapHeight / 30000; // Scale for Y axis based on world height
+
+  // Draw mini-map background
+  fill(200, 200, 200, 150); // Semi-transparent gray
+  rect(mapX, mapY, mapWidth, mapHeight);
+
+  // Draw roads on the mini-map
+  drawMiniRoad(JumRoad, mapX, mapY, scaleX, scaleY);
+  drawMiniRoad(WaslRoad, mapX, mapY, scaleX, scaleY);
+  drawMiniRoad(ZayedRoad, mapX, mapY, scaleX, scaleY);
+  drawMiniRoad(EmiratesRoad, mapX, mapY, scaleX, scaleY);
+  drawMiniRoad(SweihanRoad, mapX, mapY, scaleX, scaleY);
+  drawMiniRoad(SultanRoad, mapX, mapY, scaleX, scaleY);
+
+  // Draw the streets
+  for (let road of [street1, street2, street3, street4, street5, street6, street7, street8, street9, street10]) {
+    drawMiniRoad(road, mapX, mapY, scaleX, scaleY);
+  }
+
+  // Draw the player on the mini-map
+  fill(255, 0, 0); // Red for the player
+  let playerMiniX = mapX + player.x * scaleX;
+  let playerMiniY = mapY + player.y * scaleY;
+  ellipse(playerMiniX, playerMiniY, 5, 5); // Small dot for the player
+
+  // Draw the car if the player is in it
+  if (inCar) {
+    fill(0, 0, 255); // Blue for the car
+    let carMiniX = mapX + ferrari.x * scaleX;
+    let carMiniY = mapY + ferrari.y * scaleY;
+    ellipse(carMiniX, carMiniY, 5, 5); // Small dot for the car
+  }
+}
+
+// Helper function to draw roads on the mini-map
+function drawMiniRoad(road, mapX, mapY, scaleX, scaleY) {
+  let roadMiniX = mapX + road.x * scaleX;
+  let roadMiniY = mapY + road.y * scaleY;
+  let roadMiniWidth = road.width * scaleX;
+  let roadMiniHeight = road.height * scaleY;
+
+  // Draw the road only if it's inside the mini-map boundaries
+  if (
+    roadMiniX + roadMiniWidth > mapX && // Road is partially or fully within mini-map X bounds
+    roadMiniX < mapX + 200 && // Road is partially or fully within mini-map X bounds
+    roadMiniY + roadMiniHeight > mapY && // Road is partially or fully within mini-map Y bounds
+    roadMiniY < mapY + 150 // Road is partially or fully within mini-map Y bounds
+  ) {
+    rect(roadMiniX, roadMiniY, roadMiniWidth, roadMiniHeight);
+  }
+}
